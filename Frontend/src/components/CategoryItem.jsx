9cbb1ed6e3265f218bc51c328bbee94f875e3906
Apisathan <apisathan2000@gmail.com>
2023-03-05 07:56:46 +0530

    Started working on Categories pages

---
+++ Frontend/src/components/CategoryItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {styled} from "@material-ui/core"
+
+const Container = styled.div``
+
+const CategoryItem = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default CategoryItem
