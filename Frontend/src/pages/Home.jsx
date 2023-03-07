1685bd3b341d88b396c64f2e6ceb6795420a5e01
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-03-07 13:43:15 +0530

    fix: fix a bug in Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
