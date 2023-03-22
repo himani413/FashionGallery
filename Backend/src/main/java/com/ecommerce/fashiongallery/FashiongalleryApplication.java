9c0e9f2e56ad3d40ce0de646ef717dc0dead00fe
Dinuka_Ekanayake <dinukaekanayaka18@gail.com>
2023-03-22 13:21:01 +0530

    initial commit

---
+++ Backend/src/main/java/com/ecommerce/fashiongallery/FashiongalleryApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.fashiongallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashiongalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashiongalleryApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ecommerce/fashiongallery/FashiongalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.fashiongallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashiongalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
