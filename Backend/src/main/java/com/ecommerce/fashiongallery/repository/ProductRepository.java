ed6d1dfca0ca16b877f3f7fe0d7a5d8631c3146c
MMPjayarathne <pramudithajayarathna@gmail.com>
2023-03-25 20:44:48 +0530

    feat:created repositories and edited entities

---
+++ Backend/src/main/java/com/ecommerce/fashiongallery/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.fashiongallery.repository;
+
+
+import com.ecommerce.fashiongallery.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
