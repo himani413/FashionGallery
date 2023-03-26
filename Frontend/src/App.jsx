515cdd8429171691185367c2b826f2bef585ba30
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-03-26 09:20:31 +0530

    feat: complete ProductList page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Home/>;
+  return <ProductList/>;
 };
 
 export default App;
--- Frontend/src/pages/ProductList.jsx
+++ Frontend/src/pages/ProductList.jsx
@@ -1,40 +1,11 @@
-import styled from "styled-components"
 import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement"
-
+import Products from "../components/Products"
 import Newsletter from "../components/Newsletter"
 import Footer from "../components/Footer"
 import Copyright from "../components/Copyright"
-
-const Container = styled.div``
-
-const Title = styled.h1`
-    margin: 20px;
-`
-
-const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-`
-
-const Filter = styled.div`
-    margin: 20px;
-`
-
-const FilterText = styled.span`
-    font-size: 20px;
-    font-weight: 500;
-    margin-right: 20px;
-`
-const Select = styled.select`
-    padding: 10px;
-    margin-right: 20px;
-   
-`
-
-const Option = styled.option`
-
-`
+import {Container,Title,FilterContainer,FilterText,
+        Filter,Select,Option} from "../styles/ProductList-Styles"
 
 export const ProductList = () => {
   return (
@@ -64,7 +35,7 @@ export const ProductList = () => {
             </Filter>
             
         </FilterContainer>
-        
+        <Products/>
         <Newsletter/>
         <Footer/>
         <Copyright/>
