224063830523d73c0076a80eeb55538a2abf4832
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-03-26 20:12:08 +0530

    feat: added Address entity

---
+++ Backend/src/main/java/com/ecommerce/fashiongallery/entity/Address.java
@@ -0,0 +1,32 @@
+package com.ecommerce.fashiongallery.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "address_line_1", nullable = false, length = 512)
+    private String addressLine1;
+
+    @Column(name = "address_line_2", length = 512)
+    private String addressLine2;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Column(name = "zipcode", nullable = false)
+    private Integer zipcode;
+
+}
