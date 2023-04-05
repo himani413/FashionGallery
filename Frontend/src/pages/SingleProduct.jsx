e718ea1297d609b3664f3cd4ad419832a7fe9253
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-04-05 21:51:53 +0530

    feat: improve styles on error messages

--- Frontend/src/pages/SingleProduct.jsx
+++ Frontend/src/pages/SingleProduct.jsx
@@ -6,7 +6,7 @@ import Copyright from '../components/Copyright'
 import {Container,ImgContainer,Wrapper,Image, 
         InfoContainer, Title, Desc, Price, FilterContainer, 
         Filter, FilterTitle, FilterSize, FilterSizeOption, 
-        AddContainer, AmountContainer, Amount, Button} from '../styles/SingleProduct-Styles'
+        AddContainer, AmountContainer, Amount, Button, Message} from '../styles/SingleProduct-Styles'
 import productimg from '../images/black-jumpsuit.jpg'
 import { Add, Remove } from '@material-ui/icons'
 import axios from 'axios';
@@ -112,7 +112,7 @@ const SingleProduct = () => {
                     </AmountContainer>
                     <Button disabled={!isQuantityValid} onClick={handleAddToCart}>Add To Cart</Button>
                     <Button>Buy Now</Button>
-                    {!isQuantityValid && <h2>The selected quantity is greater than the available quantity</h2>}
+                    {!isQuantityValid && <Message>The selected quantity is greater than the available quantity!</Message>}
                   </>
                 )}
             </AddContainer>
